d5dee01df775b43006b7ffbbdc8c8427ff0714d1
Maksim Kozhin <138617398+Dare-Ka@users.noreply.github.com>
2023-08-26 08:22:24 +0300

    Add files via upload

--- BackUp VK.py
+++ BackUp VK.py
@@ -30,30 +30,33 @@ class VK:
                  'count': 50}
 
        response = requests.get(photos_get_url, params={**self.params, **params})
-       data = response.json()
+       if 200 <= response.status_code < 300:
+           data = response.json()
 
-       photos_links = []
-       info_list = []
-       names = []
-       size_types = []
-       for item in data['response']['items']:
-           if item['likes']['count'] not in names:
-               names.append(item['likes']['count'])
-           else:
-               names.append(f'{item["likes"]["count"]}_{datetime.utcfromtimestamp(item["date"]).strftime("%Y-%m-%d")}')
-           photos_links.append(item['sizes'][-1]['url'])
-           size_types.append(item['sizes'][-1]['type'])
-       global links
-       links = list(zip(names, size_types, photos_links))
-       for name, size_type, link in links:
-           name_dict = {'file_name': f'{name}.jpg', 'size': size_type}
-           info_list.append(name_dict)
-       info_file = json.dumps(info_list)
-       if not os.path.exists('images_json'):
-           os.mkdir('images_json')
-       with open('images_json/%s' % 'JSON_DATA.json', 'w') as file:
-           file.write(info_file)
-       return f'Скачано {len(names)} фотографий из ВК'
+           photos_links = []
+           info_list = []
+           names = []
+           size_types = []
+           for item in data['response']['items']:
+              if item['likes']['count'] not in names:
+                  names.append(item['likes']['count'])
+              else:
+                  names.append(f'{item["likes"]["count"]}_{datetime.utcfromtimestamp(item["date"]).strftime("%Y-%m-%d")}')
+              photos_links.append(item['sizes'][-1]['url'])
+              size_types.append(item['sizes'][-1]['type'])
+           global links
+           links = list(zip(names, size_types, photos_links))
+           for name, size_type, link in links:
+               name_dict = {'file_name': f'{name}.jpg', 'size': size_type}
+               info_list.append(name_dict)
+           info_file = json.dumps(info_list)
+           if not os.path.exists('images_json'):
+               os.mkdir('images_json')
+           with open('images_json/%s' % 'JSON_DATA.json', 'w') as file:
+               file.write(info_file)
+           return f'Скачано {len(names)} фото из ВК'
+       else:
+           return 'Возникла ошибка. Проверьте подключение к интернету или корректность ввода токена'
 
 class Yandex:
 
@@ -65,21 +68,27 @@ class Yandex:
        self.headers = {'Content-Type': 'application/json',
                        'Accept': 'application/json',
                        'Authorization': f'OAuth {self.token}'}
-       requests.put(f'{URL}?path={path}', headers=self.headers)
-       count = 0
-       with tqdm(range(len(links))) as progress:
-           for name, size_type, link in links:
-               self.headers = {'Content-Type': 'application/json',
+       get_href = requests.put(f'{URL}?path={path}', headers=self.headers)
+       if 200<= get_href.status_code < 300:
+           count = 0
+           with tqdm(range(len(links))) as progress:
+               for name, size_type, link in links:
+                   self.headers = {'Content-Type': 'application/json',
                            'Accept': 'application/json',
                            'Authorization': f'OAuth {self.token}'}
-               self.params = {'path': f'{path}/{name}',
+                   self.params = {'path': f'{path}/{name}',
                           'overwrite': 'true'}
-               response = requests.get(f'{URL}/upload', headers=self.headers, params=self.params)
-               href = response.json().get('href')
-               requests.put(href, data=requests.get(link).content)
-               count +=1
-               progress.update()
-       return f'Загружено {count} фотографий на Яндекс Диск'
+                   response = requests.get(f'{URL}/upload', headers=self.headers, params=self.params)
+                   if 200 <= response.status_code < 300:
+                       href = response.json().get('href')
+                       requests.put(href, data=requests.get(link).content)
+                       count +=1
+                       progress.update()
+                   else:
+                       return 'Ошибка с загрузкой на Яндекс Диск. Проверьте подключение к интернету или корректность ввода токена'
+           return f'Загружено {count} фото на Яндекс Диск'
+       else:
+           return 'Ошибка с подключением к Яндекс Диску. Проверьте подключение к интернету или корректность ввода токена'
 
 load_dotenv()
 access_token = os.getenv('access_token')
